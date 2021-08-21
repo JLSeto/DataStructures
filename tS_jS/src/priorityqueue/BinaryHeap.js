93c57c2c13be2c88c8fefc350e5b5c668434eabe
jlseto <setojs0@gmail.com>
2021-08-20 23:04:08 -0700

    updates

---
+++ tS_jS/src/priorityqueue/BinaryHeap.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.BinaryHeap = void 0;
+var BinaryHeap = /** @class */ (function () {
+    function BinaryHeap(arr) {
+        //# of Elements currently inside the heap
+        this.heapSize = 0;
+        this.heap = new Array();
+    }
+    return BinaryHeap;
+}());
+exports.BinaryHeap = BinaryHeap;
